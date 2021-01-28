41328b096aa09063217aafbb55dc67d6d66e6bae
miguelsantosan <miguel.san.santos@gmail.com>
2021-01-28 19:42:01 -0300

    Implementada a primeira animação

--- threejs/anim_1.js
+++ threejs/anim_1.js
@@ -1,17 +1,36 @@
 function WaveAnimation() {}
+    var angle_in_degrees = 180;
+    var angle_in_radians = angle_in_degrees * Math.PI / 180;
 
 Object.assign( WaveAnimation.prototype, {
 
     init: function() {
-        let upperArmTween = new TWEEN.Tween( {theta:0} )
-            .to( {theta:Math.PI }, 500)
+        let upperArmTween = new TWEEN.Tween( {theta: 0} )
+            .to( {theta: angle_in_radians }, 500)
             .onUpdate(function(){
                 // This is an example of rotation of the right_upper_arm 
                 // Notice that the transform is M = T * R 
+
+                // Pivôs pra cada articulação do robô
+                var pivot_right_upper_arm, pivor_right_lower_arm, pivot_right_hand;
+
                 let right_upper_arm =  robot.getObjectByName("right_upper_arm");
-                right_upper_arm.matrix.makeRotationZ(this._object.theta).premultiply( new THREE.Matrix4().makeTranslation(2.6, 0, 0 ) );
 
+                pivot_right_upper_arm = new THREE.Vector3(
+                    right_upper_arm.position.x,
+                    right_upper_arm.position.y - right_upper_arm.geometry.parameters.height/2,
+                    right_upper_arm.position.z,
+                );
 
+                // A primeira matriz da operação será a mais à direita
+                // premultiply() vai adicionando as matriz à esquerda                
+                right_upper_arm.matrix.makeTranslation(0, pivot_right_upper_arm.y, 0).premultiply(
+                    new THREE.Matrix4().makeRotationZ(this._object.theta).premultiply(
+                    new THREE.Matrix4().makeTranslation(pivot_right_upper_arm.x, -pivot_right_upper_arm.y, 0 ) ));
+                
+
+                // Rotação original
+                //right_upper_arm.matrix.makeRotationZ(this._object.theta).premultiply( new THREE.Matrix4().makeTranslation(2.6, 0, 0 ) );
 
                 // Updating final world matrix (with parent transforms) - mandatory
                 right_upper_arm.updateMatrixWorld(true);
